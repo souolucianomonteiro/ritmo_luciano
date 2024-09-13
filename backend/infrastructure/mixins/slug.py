e5ae09b54282ab23accdd6785f99b459f5330cfc
Luciano <dlucianoveronese@dbpm.com.br>
2024-09-13 09:50:39 -0300

    Adicionando mixins para slug e status

---
+++ backend/infrastructure/mixins/slug.py
@@ -0,0 +1,57 @@
+"""
+Mixin que adiciona um campo de slug e métodos utilitários relacionados
+para um modelo Django.
+
+Este mixin inclui um campo `slug` que é utilizado para gerar URLs amigáveis
+com base em outro campo do modelo, como `nome` ou `título`. Ele também
+fornece métodos para automaticamente preencher o campo `slug` a partir de
+um valor dado e garantir que o slug seja único.
+
+Atributos:
+    slug (models.SlugField): Campo de slug que será gerado automaticamente
+    com base em um campo específico do modelo.
+
+Métodos:
+    save(*args, **kwargs): Sobrescreve o método save do modelo para garantir
+    que o campo slug seja gerado e validado antes de salvar o modelo.
+    _generate_slug(): Gera o valor do slug baseado em um campo específico
+    do modelo e garante sua unicidade.
+
+Exemplo de uso:
+    class Artigo(SlugMixin, models.Model):
+        titulo = models.CharField(max_length=255)
+        conteudo = models.TextField()
+
+        def __str__(self):
+            return self.titulo
+
+    # Neste exemplo, o slug será gerado automaticamente a partir do título
+    do artigo e será único.
+"""
+from django.db import models
+from django.utils.text import slugify
+
+
+class SlugMixin(models.Model):
+    """
+    Mixin que adiciona um campo slug para URLs amigáveis em modelos Django.
+    """
+
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Se o slug ainda não foi definido, cria um com base no campo 'nome'
+        # Ajuste o campo 'nome' conforme necessário
+        if not self.slug:
+            self.slug = slugify(self.nome)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        """
+        Metadados para a classe modelo.
+
+        Define que a classe é abstrata, ou seja, não será criada uma tabela
+        diretamente para este modelo no banco de dados. Outras classes podem
+        herdar este modelo e estender sua funcionalidade.
+        """
+        abstract = True
